6c51ad225a5f1fc2b124f83a5ad3ab8f03fad801
carsonzhang <60562329+lanxin-zhang@users.noreply.github.com>
2020-12-19 21:28:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,335 @@
+function Emmy2020Ballot1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("button1");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("button2");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("button3");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot4() {
+    var dots = document.getElementById("dots4");
+    var moreText = document.getElementById("more4");
+    var btnText = document.getElementById("button4");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot5() {
+    var dots = document.getElementById("dots5");
+    var moreText = document.getElementById("more5");
+    var btnText = document.getElementById("button5");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot6() {
+    var dots = document.getElementById("dots6");
+    var moreText = document.getElementById("more6");
+    var btnText = document.getElementById("button6");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot7() {
+    var dots = document.getElementById("dots7");
+    var moreText = document.getElementById("more7");
+    var btnText = document.getElementById("button7");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot8() {
+    var dots = document.getElementById("dots8");
+    var moreText = document.getElementById("more8");
+    var btnText = document.getElementById("button8");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot9() {
+    var dots = document.getElementById("dots9");
+    var moreText = document.getElementById("more9");
+    var btnText = document.getElementById("button9");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot10() {
+    var dots = document.getElementById("dots10");
+    var moreText = document.getElementById("more10");
+    var btnText = document.getElementById("button10");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot11() {
+    var dots = document.getElementById("dots11");
+    var moreText = document.getElementById("more11");
+    var btnText = document.getElementById("button11");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot12() {
+    var dots = document.getElementById("dots12");
+    var moreText = document.getElementById("more12");
+    var btnText = document.getElementById("button12");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot13() {
+    var dots = document.getElementById("dots13");
+    var moreText = document.getElementById("more13");
+    var btnText = document.getElementById("button13");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot14() {
+    var dots = document.getElementById("dots14");
+    var moreText = document.getElementById("more14");
+    var btnText = document.getElementById("button14");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot15() {
+    var dots = document.getElementById("dots15");
+    var moreText = document.getElementById("more15");
+    var btnText = document.getElementById("button15");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot16() {
+    var dots = document.getElementById("dots16");
+    var moreText = document.getElementById("more16");
+    var btnText = document.getElementById("button16");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot17() {
+    var dots = document.getElementById("dots17");
+    var moreText = document.getElementById("more17");
+    var btnText = document.getElementById("button17");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot18() {
+    var dots = document.getElementById("dots18");
+    var moreText = document.getElementById("more18");
+    var btnText = document.getElementById("button18");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot19() {
+    var dots = document.getElementById("dots19");
+    var moreText = document.getElementById("more19");
+    var btnText = document.getElementById("button19");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot20() {
+    var dots = document.getElementById("dots20");
+    var moreText = document.getElementById("more20");
+    var btnText = document.getElementById("button20");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
+
+function Emmy2020Ballot21() {
+    var dots = document.getElementById("dots21");
+    var moreText = document.getElementById("more21");
+    var btnText = document.getElementById("button21");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Press to unleash the rant:";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Press to shut me up:";
+      moreText.style.display = "inline";
+    }
+}
